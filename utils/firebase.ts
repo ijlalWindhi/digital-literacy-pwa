8dbfbc3dceda8060847622f444efcf676308f0ac
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-02-10 20:30:22 +0700

    feat(firebase): update Firebase configuration and initialize Firestore and Auth services

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,19 +1,30 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+//   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+// };
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAuPxVPXckk9-gqMkhlx6-oI0EohSkho_8",
+  authDomain: "digital-literacy-7701a.firebaseapp.com",
+  projectId: "digital-literacy-7701a",
+  storageBucket: "digital-literacy-7701a.firebasestorage.app",
+  messagingSenderId: "342930311660",
+  appId: "1:342930311660:web:455dc1e16887bece2318c9",
+  measurementId: "G-WXL66Y7XND",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
