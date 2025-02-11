930f85459ec481dad9e27b722943cfc9344d3552
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-02-11 16:55:42 +0700

    fix(active-tab): enhance URL matching for active tab detection using regex

--- utils/active-tab.ts
+++ utils/active-tab.ts
@@ -5,5 +5,10 @@ import MENU from "@/constants/menu";
 export const useActiveTab = () => {
   const pathname = usePathname();
   const allItems = [...MENU.navSecondary, ...MENU.menu, ...MENU.other];
-  return allItems.find((item) => item.url === pathname);
+  return allItems.find((item) => {
+    const regex = new RegExp(
+      `^${item.url.replace(/\{dynamic\}/g, "[^/]+").replace(/\//g, "\\/")}$`,
+    );
+    return regex.test(pathname);
+  });
 };
