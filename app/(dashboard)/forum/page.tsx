b1a54bed03ecff36920bd8bd6639e8031168a7a5
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-02-11 15:18:17 +0700

    feat(forum): create Forum component and integrate it into ForumPage

--- app/(dashboard)/forum/page.tsx
+++ app/(dashboard)/forum/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Metadata } from "next";
 
+import Forum from "@/features/forum";
+
 import { METADATA } from "@/constants/metadata";
 
 export const metadata: Metadata = {
@@ -12,5 +14,5 @@ export const metadata: Metadata = {
 };
 
 export default function ForumPage() {
-  return <div>ForumPage</div>;
+  return <Forum />;
 }
