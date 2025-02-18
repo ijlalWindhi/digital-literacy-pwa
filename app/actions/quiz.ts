e3d2e719bccf2f1581f3a626a7616792ec28f187
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2025-02-18 20:51:01 +0700

    feat(quiz): add functions to fetch quizzes and recent quizzes from Firestore

---
+++ app/actions/quiz.ts
@@ -0,0 +1,52 @@
+"use server";
+
+import { db } from "@/utils/firebase";
+import {
+  collection,
+  query,
+  where,
+  orderBy,
+  getDocs,
+  limit,
+} from "firebase/firestore";
+import { TQuiz, TQuizCategory } from "@/types";
+
+const quizzesCollection = collection(db, "quizs");
+
+export async function getQuizzes(category?: TQuizCategory) {
+  try {
+    let quizQuery = query(quizzesCollection, orderBy("created_at", "desc"));
+
+    if (category) {
+      quizQuery = query(
+        quizzesCollection,
+        where("category.id", "==", category),
+        orderBy("created_at", "desc"),
+      );
+    }
+
+    const snapshot = await getDocs(quizQuery);
+    return snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    })) as TQuiz[];
+  } catch (error) {
+    throw new Error(
+      error instanceof Error ? error.message : "An unknown error occurred",
+    );
+  }
+}
+
+export async function getRecentQuizzes(limitCount = 3) {
+  const q = query(
+    quizzesCollection,
+    orderBy("created_at", "desc"),
+    limit(limitCount),
+  );
+
+  const snapshot = await getDocs(q);
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as TQuiz[];
+}
